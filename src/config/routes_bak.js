f445708db5bbfc4d9774292016fbdecd0776b7f1
sober <cp_125@163.com>
2019-03-19 13:52:48 +0800

    aff

--- src/config/routes_bak.js
+++ src/config/routes_bak.js
@@ -713,7 +713,12 @@ export default [
 				iconCls: '',
 				name: '代理提款'
 			},
-
+            {
+				path: '/dailyWages',
+				component: resolve => require(['../page/agent/dailyWages'], resolve),
+				iconCls: '',
+				name: '日工资管理'
+			}
 		]
 	},
 	{
---
+++ src/page/agent/dailyWages.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="dailyWages">
+        我是代理日工资
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			init() {
+
+			}
+		},
+		components: {},
+		computed: {},
+		watch: {},
+		mounted() {
+		},
+		created() {
+
+		}
+	}
+</script>
+<style scopend>
+
+</style>
